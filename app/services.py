7135665e549653a0e0780af41b5f359756f23197
emrecancc <emrecancerli55@gmail.com>
2026-07-03 00:37:05 +0300

    chore: devmend retry hotfix #53

--- app/services.py
+++ app/services.py
@@ -1,8 +1,23 @@
-from typing import Optional
+from app.models import User
+from sqlalchemy.orm import Session
 
 class UserService:
-    def __init__(self, db):
+    def __init__(self, db: Session):
         self.db = db
 
     def get_user(self, user_id):
-        return self.db.get_user(user_id)
+        return self.db.query(User).filter(User.id == user_id).first()
+
+    def create_user(self, user_data):
+        user = User(**user_data)
+        self.db.add(user)
+        self.db.commit()
+        self.db.refresh(user)
+        return user
+
+    def delete_user(self, user_id):
+        user = self.get_user(user_id)
+        if user:
+            self.db.delete(user)
+            self.db.commit()
+        return user
