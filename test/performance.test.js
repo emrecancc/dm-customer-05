1ba40e3e1b6f1a31a021347ae60fc8547e902a7f
emrecancc <emrecancerli55@gmail.com>
2026-07-03 00:45:14 +0300

    chore: devmend retry hotfix #62

--- test/performance.test.js
+++ test/performance.test.js
@@ -1,11 +1,11 @@
-import request from 'supertest';
-import app from '../src/app';
+const request = require('supertest');
+const app = require('../app');
 
-describe('Performance tests', () => {
-  it('API responds within 1250ms', async () => {
+describe('API performance', () => {
+  it('responds within 400ms', async () => {
     const start = Date.now();
-    await request(app).get('/api/endpoint');
+    await request(app).get('/');
     const duration = Date.now() - start;
-    expect(duration).toBeLessThan(1250);
+    expect(duration).toBeLessThan(400);
   });
 });
